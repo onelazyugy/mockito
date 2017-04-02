6e08a24ab3189389379ea0dfb11f82d346951ea4
Viet <viettangle12@gmail.com>
2017-04-02 19:10:02 -0400

    added test for Tiger

--- src/test/java/com/le/viet/mockito/mock/TigerTest.java
+++ src/test/java/com/le/viet/mockito/mock/TigerTest.java
@@ -1,13 +1,28 @@
 package com.le.viet.mockito.mock;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Created by onelazyguy on 4/2/17.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TigerTest {
-    @Test
-    void testTigerMakeNoise(){
+    @Mock
+    private Animal tigerMock;
 
+    @Test
+    public void testTigerMakeNoise(){
+        String acutalNoise = "rgghh rgghh";
+        tigerMock = mock(Tiger.class);
+        when(tigerMock.makeNoise()).thenReturn(acutalNoise);
+        //verify(tigerMock).makeNoise();
+        String expectedNoise = tigerMock.makeNoise();
+        Assert.assertEquals(acutalNoise, expectedNoise);
     }
 }
